4abfcce90edf8add018c144a69765f4649170f47
Richmond Fernandez <escabasjeff@gmail.com>
2024-12-05 11:14:18 +0800

    changes

--- add-template.php
+++ add-template.php
@@ -1,45 +1,28 @@
 <?php include_once __DIR__ . '/header.php'; ?>
 		
-		<!--**********************************
-            Content body start
-        ***********************************-->
-        <div class="content-body">
-			<div class="container-fluid">
-				<!-- Row -->
-				<div class="row">
-					<div class="col-xl-12">
-                        <!-- Row -->
-						<div class="row">
-                            <!--column-->
-							<div class="col-xl-12">
-								<div class="page-title flex-wrap">
-                                    <div>
-										<h4>Add Lesson Plan Templates</h4>
-									</div>
+	<!--**********************************
+		Content body start
+	***********************************-->
+	<div class="content-body">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="row">
+						<div class="col-xl-12">
+							<div class="page-title flex-wrap">
+								<div>
+									<h4>Add Lesson Plan Templates</h4>
 								</div>
 							</div>
-                        	<!--/Row -->
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		
-        <!--**********************************
-            Content body end
-        ***********************************-->
-
-		<!--**********************************
-           Support ticket button start
-        ***********************************-->
-		
-        <!--**********************************
-           Support ticket button end
-        ***********************************-->
-
-
 	</div>
-    <!--**********************************
-        Main wrapper end
-    ***********************************-->
+		
+	<!--**********************************
+		Content body end
+	***********************************-->
+
 <?php include_once __DIR__ . '/footer.php'; ?>
--- view-template.php
+++ view-template.php
@@ -1,131 +1,97 @@
 <?php include_once __DIR__ . '/header.php'; ?>
 		
-		<!--**********************************
-            Content body start
-        ***********************************-->
-        <div class="content-body">
-			<div class="container-fluid">
-				<!-- Row -->
-				<div class="row">
-					<div class="col-xl-12">
-                        <!-- Row -->
-						<div class="row">
-                            <!--column-->
-							<div class="col-xl-12">
-								<div class="page-title flex-wrap">
-                                    <div>
-										<h4>Daily Lesson Plan Templates</h4>
-									</div>
-								</div>
-							</div>
-                            <!--/column-->
-                            <!--column-->
-                            <div class="col-xl-12">
-                                 <!-- Row -->
-                                <div class="row">
-                                    <!--column-->
-                                    <div class="col-xl-3 col-lg-4 col-sm-6">
-                                        <div class="card contact_list text-center">
-                                            <div class="card-body">
-                                                <div class="user-content">
-                                                    <div class="user-info">
-                                                        <div class="user-details">
-                                                            <h4 class="user-name mb-0">Classic</h4>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-								</div>	
-							<!--/column-->
-							</div>
-
-                            <div class="col-xl-12">
-								<div class="page-title flex-wrap">
-                                    <div>
-										<h4>Weekly Lesson Plan Templates</h4>
-									</div>
-								</div>
-							</div>
-                            <!--/column-->
-                            <!--column-->
-                            <div class="col-xl-12">
-                                 <!-- Row -->
-                                <div class="row">
-                                    <!--column-->
-                                    <div class="col-xl-3 col-lg-4 col-sm-6">
-                                        <div class="card contact_list text-center">
-                                            <div class="card-body">
-                                                <div class="user-content">
-                                                    <div class="user-info">
-                                                        <div class="user-details">
-                                                            <h4 class="user-name mb-0">Classic</h4>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-								</div>	
-							<!--/column-->
-							</div>
-
-                            <div class="col-xl-12">
-								<div class="page-title flex-wrap">
-                                    <div>
-										<h4>Monthly Lesson Plan Templates</h4>
-									</div>
-								</div>
-							</div>
-                            <!--/column-->
-                            <!--column-->
-                            <div class="col-xl-12">
-                                 <!-- Row -->
-                                <div class="row">
-                                    <!--column-->
-                                    <div class="col-xl-3 col-lg-4 col-sm-6">
-                                        <div class="card contact_list text-center">
-                                            <div class="card-body">
-                                                <div class="user-content">
-                                                    <div class="user-info">
-                                                        <div class="user-details">
-                                                            <h4 class="user-name mb-0">Classic</h4>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-								</div>	
-							<!--/column-->
-							</div>
-                           
-                        	<!--/Row -->
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-        <!--**********************************
-            Content body end
-        ***********************************-->
-
-		<!--**********************************
-           Support ticket button start
-        ***********************************-->
-		
-        <!--**********************************
-           Support ticket button end
-        ***********************************-->
-
-
-	</div>
     <!--**********************************
-        Main wrapper end
+        Content body start
     ***********************************-->
+    <div class="content-body">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="page-title flex-wrap">
+                                <div>
+                                    <h4>Daily Lesson Plan Templates</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-12">
+                            <div class="row">
+                                <div class="col-xl-3 col-lg-4 col-sm-6">
+                                    <div class="card contact_list text-center">
+                                        <div class="card-body">
+                                            <div class="user-content">
+                                                <div class="user-info">
+                                                    <div class="user-details">
+                                                        <h4 class="user-name mb-0">Classic</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>	
+                        <!--/column-->
+                        </div>
+
+                        <div class="col-xl-12">
+                            <div class="page-title flex-wrap">
+                                <div>
+                                    <h4>Weekly Lesson Plan Templates</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-12">
+                            <div class="row">
+                                <div class="col-xl-3 col-lg-4 col-sm-6">
+                                    <div class="card contact_list text-center">
+                                        <div class="card-body">
+                                            <div class="user-content">
+                                                <div class="user-info">
+                                                    <div class="user-details">
+                                                        <h4 class="user-name mb-0">Classic</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>	
+                        </div>
+
+                        <div class="col-xl-12">
+                            <div class="page-title flex-wrap">
+                                <div>
+                                    <h4>Monthly Lesson Plan Templates</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-12">
+                            <div class="row">
+                                <div class="col-xl-3 col-lg-4 col-sm-6">
+                                    <div class="card contact_list text-center">
+                                        <div class="card-body">
+                                            <div class="user-content">
+                                                <div class="user-info">
+                                                    <div class="user-details">
+                                                        <h4 class="user-name mb-0">Classic</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>	
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--**********************************
+        Content body end
+    ***********************************-->
+
 <?php include_once __DIR__ . '/footer.php'; ?>
